624cf2284d16f92e231c3ae1824c7882db6e0433
Lucas Martins <32624827+lucasufc@users.noreply.github.com>
2021-10-25 09:50:14 -0300

    Delete Cod3r/Exercicios1 js directory

--- Cod3r/Exercicios1 js/questao24.js
+++
@@ -1,9 +0,0 @@
-/*
- * Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
- */
-
-let contador = 1
-while(contador < 12){
-    console.log("Hello Wordl")
-    ++contador
-}
--- Cod3r/Exercicios1 js/questao25.js
+++
@@ -1,9 +0,0 @@
-/*
- * Escrever um programa para exibir os números de 1 até 50 na tela
- */
-
-let contador = 1
-while(contador < 51){
-    console.log(contador)
-    ++contador
-}
