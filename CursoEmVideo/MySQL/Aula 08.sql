2bae35416c10e8b154e981c840c61928485192aa
Lucas Martins <lucashk.eng@gmail.com>
2021-11-23 20:21:50 -0300

    SQL

---
+++ CursoEmVideo/MySQL/Aula 08.sql
@@ -0,0 +1,11 @@
+use cadastro;
+
+select * from cursos;
+select * from gafanhotos;
+
+describe gafanhotos;
+desc cursos;
+
+drop database cadastro;
+
+## No MySQL Workbench ir em Server > Data Export / Import
