460f872df19a4178d05522c708a77b1527131631
Lucas Martins <lucashk.eng@gmail.com>
2021-11-26 09:45:50 -0300

    Create Aula 09.sql

---
+++ CursoEmVideo/MySQL/Aula 09.sql
@@ -0,0 +1,14 @@
+#No console
+show databases;
+
+use cadastro;
+show tables;
+
+desc gafanhotos;
+
+select * from gafanhotos;
+select * from crusos;
+
+update cursos set nome = 'Ph' where idcurso = '4';
+
+select * from crusos;
