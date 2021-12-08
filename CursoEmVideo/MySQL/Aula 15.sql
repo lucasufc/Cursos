7d784b2f978cb278fdef581667efc9126806fc82
Lucas Martins <lucashk.eng@gmail.com>
2021-12-08 16:06:47 -0300

    Create Aula 15.sql

---
+++ CursoEmVideo/MySQL/Aula 15.sql
@@ -0,0 +1,33 @@
+use cadastro;
+describe gafanhotos;
+
+alter table gafanhotos
+	add column cursopreferido int;
+
+alter table gafanhotos
+	add foreign key(cursopreferido)
+	references cursos(idcurso);
+    
+select * from gafanhotos;
+select * from cursos;
+
+update gafanhotos set cursopreferido = '6' where id = '1';
+#não pode pois o curso 6 já está sendo usado
+delete from cursos where (idcurso = '6');
+
+#pode pois ninguem usa o curso 16
+delete from cursos where (idcurso = '28');
+
+select nome, cursopreferido from gafanhotos;
+
+select nome, ano from cursos;
+
+select gafanhotos.nome, cursos.nome, cursos.ano
+	from gafanhotos join cursos
+    on cursos.idcurso = gafanhotos.cursopreferido
+    order by gafanhotos.nome;
+
+select g.nome, c.nome, c.ano
+	from gafanhotos as g  inner join cursos as c
+    on c.idcurso = g.cursopreferido
+    order by g.nome;
