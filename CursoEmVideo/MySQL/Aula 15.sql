45db86f127609461a30fa66ad014384d6fab4b08
Lucas Martins <lucashk.eng@gmail.com>
2021-12-08 16:07:17 -0300

    Update Aula 15.sql

--- CursoEmVideo/MySQL/Aula 15.sql
+++ CursoEmVideo/MySQL/Aula 15.sql
@@ -31,3 +31,13 @@ select g.nome, c.nome, c.ano
 	from gafanhotos as g  inner join cursos as c
     on c.idcurso = g.cursopreferido
     order by g.nome;
+    
+select g.nome, c.nome, c.ano
+	from gafanhotos as g  left outer join cursos as c
+    on c.idcurso = g.cursopreferido
+    order by g.nome;
+    
+select g.nome, c.nome, c.ano
+	from gafanhotos as g  right outer join cursos as c
+    on c.idcurso = g.cursopreferido
+    order by g.nome;
