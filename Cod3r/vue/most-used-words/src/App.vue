928688c222e9e3752f3ee8c204e66693cbcf49b6
Lucas Martins <lucashk.eng@gmail.com>
2022-01-02 20:11:46 -0300

    Começo do Curso de Angular

---
+++ Cod3r/vue/most-used-words/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <v-app-bar app color="#dc143c" dark>
+            <v-toolbar-title>Most Used Words</v-toolbar-title>
+        </v-app-bar>
+        <v-main>
+          <Home />
+      </v-main>
+    </v-app>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Home
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ Cod3r/vue/most-used-words/src/components/Pill.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="pill">
+        <span class="name">{{ name }}</span>
+        <span class="amount">{{ amount }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'amount']
+}
+</script>
+
+<style scoped>
+    .pill {
+        margin: 10px;
+        height: 55px;
+        border-radius: 30px;
+
+        background-color: #dc143c;
+        color: #ffffff;
+        font-size: 1.3rem;
+        font-weight: bold;
+
+        display: flex;
+        align-items: center;
+    }
+
+    .pill .name {
+        margin-left: 15px;
+
+    }
+    .pill .amount {
+        margin: 10px;
+        padding: 15px;
+        height: 50px;
+        width: 50px;
+        border-radius: 50%;
+        
+        background-color: #fff;
+        color: #000;
+        font-size: 14px;
+
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
+        align-items: center;
+    }
+</style>
