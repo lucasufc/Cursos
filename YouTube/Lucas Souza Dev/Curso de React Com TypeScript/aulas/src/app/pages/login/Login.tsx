e1765557c93f6f2113580ed7d2850328afdcedd7
Lucas Martins <lucashk.eng@gmail.com>
2022-01-09 17:58:57 -0300

    feat: add form to login page

--- YouTube/Lucas Souza Dev/Curso de React Com TypeScript/aulas/src/app/pages/login/Login.tsx
+++ YouTube/Lucas Souza Dev/Curso de React Com TypeScript/aulas/src/app/pages/login/Login.tsx
@@ -1,7 +1,28 @@
-
+import { useState } from 'react'
 
 export const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const handleEntrar = () => {
+        console.log(email, password)
+    }
+
     return(
-        <p>Login</p>
+        <div>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </label>
+
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+            </form>
+        </div>
     )
 }
