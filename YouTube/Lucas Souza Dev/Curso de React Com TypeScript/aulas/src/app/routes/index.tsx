8bd4e28544e566ac5d93a457a48ad0995dd16597
Lucas Martins <lucashk.eng@gmail.com>
2022-01-09 14:30:09 -0300

    feat: add dashboard page, login page and react router dom

---
+++ YouTube/Lucas Souza Dev/Curso de React Com TypeScript/aulas/src/app/routes/index.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Navigate, Route, Routes as Switch, } from "react-router-dom"
+import { Dashboard, Login } from "../pages"
+
+export const Routes = () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/entrar" element={<Login/>}/>
+                <Route path="/pagina-inicial" element={<Dashboard/>}/>
+
+                <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
