dd7876947f563c5b008fa3e29c374bf04132b35e
Lucas Martins <lucashk.eng@gmail.com>
2022-02-11 20:14:02 -0300

    Desafio 006

---
+++ CursoEmVideo/Python/Modulo 01/exercicios/ex006.py
@@ -0,0 +1,8 @@
+numero = float(input('Digite o número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz_quadrada = pow(numero,(1/2))
+print('Numero: {}'.format(numero))
+print('Dobro: {}'.format(dobro))
+print('Triplo: {}'.format(triplo))
+print('Raiz Quadrada: {}'.format(raiz_quadrada))
