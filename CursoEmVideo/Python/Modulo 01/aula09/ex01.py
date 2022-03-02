746d05fa0c552bde2bd9151b118d6b2884191f32
Lucas Martins <lucashk.eng@gmail.com>
2022-03-02 12:12:30 -0300

    Create ex01.py

---
+++ CursoEmVideo/Python/Modulo 01/aula09/ex01.py
@@ -0,0 +1,7 @@
+frase = 'Curso em Vídeo Python'
+frase = frase.replace('Python', 'Android')
+print(frase[:46])
+print('Curso' in frase)
+print(frase.lower().find('video'))
+dividido = frase.split()
+print(dividido)
